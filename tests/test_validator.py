d891f54bf61d84c8c32fc5361e507f6e4d9803cd
VladaM <vladaa905@gmail.com>
2025-02-22 16:56:26 +0300

    add number validator and tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -4,13 +4,20 @@ from validator.validator import Validator
 def test_none_value():
     v = Validator()
     schema = v.string()
-    schema2 = v.string()
-    schema2.required()
     assert schema.is_valid('') 
     assert schema.is_valid(None)
+
+    schema2 = v.string()
+    schema2.required()
     assert not schema2.is_valid('') 
     assert not schema2.is_valid(None)
     assert schema2.is_valid('what does the fox say')
+
+    schema3 = v.number()
+    assert schema3.is_valid(0)
+    schema3.required()
+    assert schema3.is_valid(0)
+    assert not schema3.is_valid(None)  
 
 
 def test_min_len():
@@ -28,3 +35,17 @@ def test_contains():
 def test_priority():
     v = Validator()
     assert v.string().min_len(10).min_len(4).is_valid('Hexlet')
+
+
+def test_positive():
+    v = Validator()
+    assert v.number().positive().is_valid(10)
+    assert not v.number().positive().is_valid(-7)
+
+
+def test_range():
+    v = Validator()
+    schema = v.number()
+    schema.range(-5, 5)
+    assert not schema.is_valid(-7) 
+    assert schema.is_valid(5)
--- validator/validator.py
+++ validator/validator.py
@@ -2,7 +2,10 @@ class Validator:
     def string(self):
         return StringValidator()
     
-    
+    def number(self):
+        return NumberValidator()
+
+
 class StringValidator:
     def __init__(self):
         self.rules = {'required': False, 'min_len': None, 'contains': None}
@@ -36,3 +39,41 @@ class StringValidator:
             return False 
 
         return True
+
+
+class NumberValidator:
+    def __init__(self):
+        self.rules = {'required': False, 'positive': False, 'range': None}
+
+    def required(self):
+        self.rules['required'] = True
+        return self
+    
+    def positive(self):
+        self.rules['positive'] = True
+        return self
+    
+    def range(self, start, end):
+        self.rules['range'] = [start, end]
+
+    def is_valid(self, number):
+        if number is None:
+            return not self.rules['required']
+    
+        if not isinstance(number, int):
+            return False
+        
+        if self.rules['required'] and not number and number != 0:
+            return False
+            
+        if self.rules['positive'] and number < 0:
+            return False
+            
+        if self.rules['range'] and number not in self.rules['range']:
+            return False 
+
+        return True
+
+    
+    
+
